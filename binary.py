82ee1677ef7352e9ca4467c3d4bc6c40224eef2a
2006avni <avnimeshram2@gmail.com>
2026-03-03 22:39:18 +0530

    Delete binary.py

--- binary.py
+++
@@ -1,7 +0,0 @@
-binary=list(input("enter a binary no"))
-value=0
-for i in range(len(binary)):
-    digit=binary.pop()
-    if digit=="1":
-        value=value+pow(2,i)
-print("The decimal value of the num is=",value)        
