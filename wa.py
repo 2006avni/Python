e7152150efa82e5f122dc8024d5d57c054ea48b0
2006avni <avnimeshram2@gmail.com>
2026-03-03 22:27:54 +0530

    Delete wa.py

--- wa.py
+++
@@ -1,7 +0,0 @@
-with open('anvy.txt', 'w') as file:
-    file.write("This is the initial content.\n")
-
-with open('anvy.txt', 'a') as file:
-    file.write("This is the appended content.\n")
-
-print("File has been written and appended successfully.")
